bbc7a213d3e3df4ee556af05282e4581b4c63f7d
Vidotti <vidottizzz@gmail.com>
2023-04-14 13:29:01 -0300

    upgraded css and some JS, added J-query to the project

--- script/script.js
+++ script/script.js
@@ -1,27 +1,35 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
+$(function () {
+  $(".range input").on("mousemove", function () {
+    var getValRange = $(this).val();
+    $(".range span").text(getValRange + "%");
+  });
+
+});
+// preencher com a cor verde até onde a bola de referencia do range input está
+const slider = document.getElementById("myinput");
+const min = slider.min;
+const max = slider.max;
+const value = slider.value;
+
+slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(value-min)/(max-min)*100}%, #DEE2E6 ${(value-min)/(max-min)*100}%, #DEE2E6 100%)`;
 
 slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-
-
-
+  this.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`;
+};
 
 // toggle
 
-let active = false
+let active = false;
 
 function toggle() {
-    let toggle = document.querySelector('.toggle')
-    let text = document.querySelector('.text')
-    active = !active
-    if (active) {
-        toggle.classList.add('active')
-        text.innerHTML = 'N'
-    } else {
-        toggle.classList.remove('active')
-        text.innerHTML = 'FF'
-    }
+  let toggle = document.querySelector(".toggle");
+  let text = document.querySelector(".text");
+  active = !active;
+  if (active) {
+    toggle.classList.add("active");
+    text.innerHTML = "N";
+  } else {
+    toggle.classList.remove("active");
+    text.innerHTML = "FF";
+  }
 }
